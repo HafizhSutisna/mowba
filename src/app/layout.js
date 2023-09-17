e401d92fe060ee70477d4af596c6067bfe8dc48d
Nakya Santini <nakya.santini@efishery.com>
2023-09-17 20:15:23 +0700

    optimize css

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import Headers from '../components/Header'
 import Footers from '../components/Footer'
 
+import StyledComponentsRegistry from '../libs/AntdRegistry'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,9 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Headers />
-        {children}
-        <Footers />
+        <StyledComponentsRegistry>
+          <Headers />
+          {children}
+          <Footers />
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
