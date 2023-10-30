f0c8bce541a7f8548f0e36c93c02e113a7f2ac5c
Nakya Santini <nakya.santini@efishery.com>
2023-10-30 18:59:46 +0700

    adjust kompetensi dasar

--- src/components/KompetensiDasar.js
+++ src/components/KompetensiDasar.js
@@ -1,6 +1,12 @@
 const KompetensiDasar = () => {
   return (
-    <div style={{ textAlign: 'start' }}>3.8 Menganalisis perkembangan kehidupan masyarakat, pemerintahan dan budaya pada masa kerajaan-kerajaan Islam di Indonesia serta menunjukkan contoh bukti-bukti yang masih berlaku pada kehidupan masyarakat Indonesia masa kini.</div>
+    <>
+      <div style={{ textAlign: 'start' }}>3.8 Menganalisis perkembangan kehidupan masyarakat, pemerintahan dan budaya pada masa kerajaan-kerajaan Islam di Indonesia serta menunjukkan contoh bukti-bukti yang masih berlaku pada kehidupan masyarakat Indonesia masa kini.</div>
+      <br />
+      <div style={{ textAlign: 'start' }}>Indikator :</div>
+      <div style={{ textAlign: 'start' }}>3.8.1 Memahami salah satu bukti peninggalan islam yang masih berlaku pada kehidupan masyarakat Indonesia masa kini.</div>
+      <div style={{ textAlign: 'start' }}>3.8.2 Menjelaskan Sejarah, Deskripsi, dan penetapan Situs Masjid Luar Batang sebagai bangunan Cagar Budaya di DKI Jakarta.</div>
+    </>
   )
 }
 
